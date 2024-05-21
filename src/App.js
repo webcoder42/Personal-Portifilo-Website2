3478d8c07ae819ebdb6057c1c7732428c30c97aa
webcoder42 <earnlearn400@gmail.com>
2024-05-21 08:57:41 +0500

    second portifilo
    done

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/mediaquery.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Web Coder</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.facebook.com/profile.php?id=100081802133117" target={"blank"}>
+            Facebook
+          </a>
+          <a href="https://www.instagram.com/bbilal420?igsh=MWNpenF5bWtjYWUweQ==" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/webcoder42?tab=repositories" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
